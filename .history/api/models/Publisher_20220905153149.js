57ce741b1f7bacea88899147dfc3c2edda701865
Your Name <franciele.fontanive@universo.univates.br>
2022-09-05 15:51:06 -0300

    adicionando editoras

---
+++ .history/api/models/Publisher_20220905153149.js
@@ -0,0 +1,33 @@
+const { DataTypes, Model } = require('sequelize');
+const db = require('../db');
+
+class Publisher extends Model { };
+
+Publisher.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cities_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'registrationCities',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize: db,
+    tableName: 'registrationPublishers',
+    modelName: 'RegistrationPublishers'
+});
+
+// City.sync({force:true})
+
+module.exports = Publisher;
---
+++ .history/api/routes/publishers_20220905154054.js
@@ -0,0 +1,24 @@
+
+const router = require('express').Router();
+const PublisherModel = require('../models/Publisher');
+const publishersController = require('../controllers/PublishersController');
+
+const validatePublisherId = async (req, res, next) => {
+  const city = await CityModel.findByPk(req.params.publisherId);
+  if (!city) {
+    return res.status(404).json({ error: 'City not found' });
+  }
+  next();
+}
+
+router.get('/publishers', publishersController.index);
+
+router.post('/publishers', publishersController.create);
+
+router.get('/publishers/:publisherId', validatePublisherId, publishersController.show);
+
+router.put('/publishers/:publisherId', validatePublisherId, publishersController.update);
+
+router.delete('/publishers/:publisherId', validatePublisherId, publishersController.delete);
+
+module.exports = router;
